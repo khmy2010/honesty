2eca0ee5ebe0cabd5deb888ec4c94d4d46b27eb0
khmy2010 <kehan.see@gmail.com>
2018-02-24 15:34:00 +0800

    logo

--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        accessToken => {
+            console.log(accessToken);
+        }
+    )
+);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+//https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=381928868316-e6ft5bu9jeq3orb4462g0c2atmcinn5h.apps.googleusercontent.com
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is listening at port ${port}`);
